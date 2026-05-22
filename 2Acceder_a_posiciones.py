d2e848dd5f479df223ce4f347c7262ffa1090480
juanser84 <juanser84@gmail.com>
2026-05-21 20:07:48 -0500

    Actividad 2

---
+++ 2Acceder_a_posiciones.py
@@ -0,0 +1,43 @@
+# Actividad 1: Crear una lista  
+
+# Creación de una lista con 5 países de Sudamérica
+
+list = ['colombia', 'ecuador', 'peru', 'venezuela', 'chile']
+
+# Imprimir cada país de la lista utilizando su índice
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+
+# imprime unicamente el primer y ultimo país de la lista
+
+# print(list[0])
+# print(list[4])
+
+# fin de la actividad 1
+
+#--------------------------------------------------------------------------
+
+# Actividad 2: Acceder a posiciones específicas de la lista
+
+# Acceder a posiciones específicas de la lista
+
+# Imprimir el pais ubicado en la posición 2
+
+print(list[2])
+
+# Intente acceder a una posición que no existe en la lista
+
+print(list[5])
+
+# Explique el error que se produce al intentar acceder a una posición que no existe en la lista
+
+# El error que se produce es un IndexError, que indica que el índice que se está intentando acceder está fuera del rango de la lista. En este caso, la lista tiene 5 elementos, por lo que los índices válidos son del 0 al 4. Al intentar acceder al índice 5, se produce el error porque no existe un elemento en esa posición.
+
+# fin de la actividad 2
+
+#--------------------------------------------------------------------------
+
