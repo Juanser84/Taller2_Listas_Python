9c78670eb63f9075befe771993a42ad2f9b137fe
juanser84 <juanser84@gmail.com>
2026-05-21 19:59:18 -0500

    first commit

---
+++ 1Creacion_de_lista.py
@@ -0,0 +1,17 @@
+# Creación de una lista con 5 países de Sudamérica
+
+list = ['colombia', 'ecuador', 'peru', 'venezuela', 'bolivia']
+
+# Imprimir cada país de la lista utilizando su índice
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+
+# imprime unicamente el primer y ultimo país de la lista
+
+print(list[0])
+print(list[4])
+
