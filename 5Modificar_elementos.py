053408df8d1534e2afa442ed5a4256b5c677669c
juanser84 <juanser84@gmail.com>
2026-05-21 20:22:33 -0500

    Actividad 5

---
+++ 5Modificar_elementos.py
@@ -0,0 +1,99 @@
+# Actividad 1: Crear una lista  
+
+# Creación de una lista con 5 países de Sudamérica
+
+list = ['colombia', 'ecuador', 'peru', 'venezuela', 'chile']
+
+# Imprimir cada país de la lista utilizando su índice
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+
+# imprime unicamente el primer y ultimo país de la lista
+
+# print(list[0])
+# print(list[4])
+
+# fin de la actividad 1
+
+#--------------------------------------------------------------------------
+
+# Actividad 2: Acceder a posiciones específicas de la lista
+
+# Acceder a posiciones específicas de la lista
+
+# Imprimir el pais ubicado en la posición 2
+
+# print(list[2])
+
+# Intente acceder a una posición que no existe en la lista
+
+# print(list[5])
+
+# Explique el error que se produce al intentar acceder a una posición que no existe en la lista
+
+# El error que se produce es un IndexError, que indica que el índice que se está intentando acceder está fuera del rango de la lista. En este caso, la lista tiene 5 elementos, por lo que los índices válidos son del 0 al 4. Al intentar acceder al índice 5, se produce el error porque no existe un elemento en esa posición.
+
+# fin de la actividad 2
+
+#--------------------------------------------------------------------------
+
+# Actividad 3: Agregar elementos a la lista
+
+# Agregar un elemento al final de la lista
+
+# list.append('argentina')
+
+# Agregar un elemento en una posición específica de la lista
+
+# list.insert(2, 'brasil')
+
+# Muestre el resultado de la lista despues de cada una de las operaciones anteriores
+
+# print(list)
+
+# fin de la actividad 3
+
+#--------------------------------------------------------------------------
+
+# Actividad 4: Eliminar elementos de la lista
+
+# Eliminar un elemento de la lista utilizando su nombre
+
+# list.remove('ecuador')
+
+# Eliminar el ultimo elemento de la lista
+
+# list.pop()
+
+# Eliminar un elemento de la lista utlizando su posición
+
+# list.pop(1)
+
+# Explique el resultado de la lista despues de cada una de las operaciones anteriores
+
+# print(list)
+
+# fin de la actividad 4
+
+#--------------------------------------------------------------------------
+
+# Actividad 5: Modificar elementos de la lista
+
+# Cambie el nombre de un país de la lista  
+
+list[2] = 'francia'
+
+# Muestre el resultado de la lista despues de la operación anterior
+
+print(list)
+
+# fin de la actividad 5
+
+#--------------------------------------------------------------------------
+
+
+
